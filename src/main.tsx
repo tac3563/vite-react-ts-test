95abcebd779fd593cad3a2b4cf2969c7d5cb1b99
tac3563 <tcox_bu@outlook.com>
2023-09-07 20:51:57 +0100

    [Update] inital commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+let num: number = 55;
+
+num = 'str';
+
+console.log(num);
